705d7a8a4c31135bb0a8f96533224677840cd44e
srikanth-nekt <srikanth@getrolo.in>
2023-06-02 16:50:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,36 @@ import streamlit as st
 import pandas as pd
 # import pyxlsb
 
-df = pd.read_csv("GG_17.csv")
-# df = pd.read_excel("GG_17.xlsb")
+import streamlit_authenticator as stauth
 
-# st.table(df)
-st.write(df)
+# from streamlit_authenticator import Authenticator
+
+import yaml
+from yaml.loader import SafeLoader
+
+with open('./config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+# authenticator = Authenticate(
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'KPI Dashboard *{name}*')
+    df = pd.read_csv("GG_17.csv")
+    # df = pd.read_excel("GG_17.xlsb")
+
+    # st.table(df)
+    st.write(df)
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
