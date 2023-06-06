ec74489b516b38e2649d4a4dfef51ef382b35473
srikanth-nekt <srikanth@getrolo.in>
2023-06-06 14:38:01 +0530

    Replace files upl

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-# import pyxlsb
+import pyxlsb
 
 import streamlit_authenticator as stauth
 
@@ -23,15 +23,25 @@ authenticator = stauth.Authenticate(
 
 name, authentication_status, username = authenticator.login('Login', 'main')
 
+is_arl = 'arl' in username
+is_sup = 'super' in username
+
 if authentication_status:
     authenticator.logout('Logout', 'main')
     st.write(f'KPI Dashboard *{name}*')
     df = pd.read_csv("GG_17.csv")
     # df = pd.read_excel("GG_17.xlsb")
-
-    # st.table(df)
-    st.write(df)
+    if is_arl:
+        st.write(df.loc[df['ARL'] == name])
+    elif is_sup:
+        st.write(df.loc[df['FL  And SM Name'] == name])
+        # print(df.columns)
+    else:
+        st.write(df)
 elif authentication_status == False:
     st.error('Username/password is incorrect')
 elif authentication_status == None:
     st.warning('Please enter your username and password')
+
+
+
